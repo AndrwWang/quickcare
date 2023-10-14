e3c22cfe859b41ef0a779c2e327de5d651f422bf
fgao63 <fgao63@gatech.edu>
2023-10-14 00:08:07 -0400

    refactoring

--- src/components/HospitalMarker.js
+++ src/components/HospitalMarker.js
@@ -2,15 +2,10 @@ export default function HospitalMarker() {
   return (
     // Important! Always set the container height explicitly
     <div style={{
-        color: 'white', 
-        background: 'grey',
-        padding: '15px 10px',
-        display: 'inline-flex',
-        textAlign: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: '100%',
-        transform: 'translate(-50%, -50%)'
+      border: "5px solid blue",
+      backgroundColor: "black",
+      position: "absolute",
+      width: "50px"
       }}>
         test
     </div>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,8 @@
 import GoogleMap from 'google-maps-react-markers';
-import HospitalMarker from './HospitalMarker';
+import React, { useRef, useState } from 'react'
+import mapOptions from '../styles/map-options.json'
+import '../styles/style.css'
+import HospitalMarker from './HospitalMarker'
 
 export default function Map() {
   const API_KEY = "AIzaSyCaE9RH_1va56W_XJ9HzdWC6h-ufMH7DZQ";
@@ -12,83 +15,52 @@ export default function Map() {
     lng: -83.387917
   }
 
-  // const HospitalPin = () => {
-  //   return <div style={{border: "5px solid blue", position: "absolute", width: "50px"}}>
-  //     test
-  //   </div>
-  // };
-  const defaultProps = {
-    center: ATLANTA,
-    zoom: 8
-  };
+	const mapRef = useRef(null)
+	const [mapReady, setMapReady] = useState(false)
+	const [mapBounds, setMapBounds] = useState({})
+	const [highlighted, setHighlighted] = useState(null)
 
-  // const renderMarkers = (map, maps) => {
-  //   let marker = new maps.Marker({
-  //   position: { lat: latitude, lng: longitude },
-  //   map,
-  //   title: 'Hello World!'
-  //   });
-  //   return marker;
-  //  };
-   
-  return (
-    // Important! Always set the container height explicitly
-    <div style={{ height: '400px', width: '400px' }}>
-      <GoogleMap
-        apiKey={API_KEY}
-        center={defaultProps.center}
-        zoom={defaultProps.zoom}
-      >
-        <HospitalMarker>
-          lat={PIEDMONT_ATHENS.lat}
-          lng={PIEDMONT_ATHENS.lng}
-        </HospitalMarker>
-      </GoogleMap>
-    </div>
-  );
+	/**
+	 * @description This function is called when the map is ready
+	 * @param {Object} map - reference to the map instance
+	 * @param {Object} maps - reference to the maps library
+	 */
+	const onGoogleApiLoaded = ({ map /* , maps */ }) => {
+		mapRef.current = map
+		setMapReady(true)
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	const onMarkerClick = (e, { markerId /* , lat, lng */ }) => {
+		setHighlighted(markerId)
+	}
+
+	const onMapChange = ({ bounds, zoom }) => {
+		const ne = bounds.getNorthEast()
+		const sw = bounds.getSouthWest()
+
+		setMapBounds({ ...mapBounds, bounds: [sw.lng(), sw.lat(), ne.lng(), ne.lat()], zoom })
+		setHighlighted(null)
+	}
+
+	return (
+		<main>
+			<div className="map-container">
+				<GoogleMap
+					apiKey={API_KEY}
+					defaultCenter={ATLANTA}
+					defaultZoom={5}
+					options={mapOptions}
+					mapMinHeight="100vh"
+					onGoogleApiLoaded={onGoogleApiLoaded}
+					onChange={onMapChange}
+				>
+					<HospitalMarker
+						lat={33.961121}
+						lng={-83.387917}
+					/>
+				</GoogleMap>
+			</div>
+		</main>
+	)
 }
-
-// const AnyReactComponent = ({ text }) => (
-//   <div style={{
-//     color: 'white', 
-//     background: 'grey',
-//     padding: '15px 10px',
-//     textAlign: 'center',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     borderRadius: '100%',
-//     transform: 'translate(-50%, -50%)'
-//   }}>
-//     {text}
-//   </div>
-// );
-
-// class SimpleMap extends React.Component {
-//   static defaultProps = {
-//     center: {lat: 59.95, lng: 30.33},
-//     zoom: 11
-//   };
-
-//   render() {
-//     return (
-//        <GoogleMapReact
-//         defaultCenter={this.props.center}
-//         defaultZoom={this.props.zoom}
-//       >
-//         <AnyReactComponent 
-//           lat={59.955413} 
-//           lng={30.337844} 
-//           text={'Kreyser Avrora'} 
-//         />
-//       </GoogleMapReact>
-//     );
-//   }
-// }
-
-
-// ReactDOM.render(
-//   <div style={{width: '100%', height: '400px'}}>
-//     <SimpleMap/>
-//   </div>,
-//   document.getElementById('main')
-// );
