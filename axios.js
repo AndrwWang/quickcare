2f55dcf41f8d8295bf400e088809cce0e135a957
Luke <19198075+LukGH4@users.noreply.github.com>
2023-10-14 01:26:32 -0400

    Edit axios.js

--- axios.js
+++ axios.js
@@ -26,6 +26,34 @@ axios.get(url)
   app.get('/api/data', (req, res) => {
     res.send(htmlContent);
   });
+
+  app.get('/api/fetch-data', async (req, res) => {
+    try {
+      // Make a server-side Axios GET request
+      const response = await axios.get('https://www.choa.org/Tools/CareCenters.aspx?v=1697249584693&_=1697249584672', {
+        headers: {
+          'Accept': 'application/json, text/javascript, */*; q=0.01',
+          'Accept-Language': 'en-US,en;q=0.9'
+        },
+      });
+  
+      // Extract and format the data
+      const emergencies = [
+        "Children's Scottish Rite Hospital: " + response.data.linked.waitTimes[5].maxWaitTime,
+        "Children's Egleston Hospital: " + response.data.linked.waitTimes[6].maxWaitTime,
+        "Children's Hughes Spalding Hospital: " + response.data.linked.waitTimes[7].maxWaitTime
+      ];
+  
+      // Log the server-side data to the console
+      console.log('Server-side data:', emergencies);
+  
+      // Send a response to the client
+      res.json(emergencies);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+  
+    }
+  });
   
   // Add any additional routes and logic as needed
   // You can also process the HTML content further using Cheerio or other libraries here
